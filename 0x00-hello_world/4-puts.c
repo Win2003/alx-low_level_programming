76a0c6228686b46380e5025b6f4ddf1a595494b3
Win2003 <winifredaniukwu@gmail.com>
2023-02-16 19:39:14 -0800

    puts-function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - prints exactly "Programming is like building a multilingual puzzle
+*followed by newline
+*Return - Always 0 (Successful)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		result 0;
+}
