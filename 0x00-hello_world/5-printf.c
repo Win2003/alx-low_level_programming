c180260576275f64f3de9f9b01165e31ce1bf07a
Win2003 <winifredaniukwu@gmail.com>
2023-02-18 19:13:37 -0800

    Using-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * purpose of the program: To print exactly "with proper grammar, but
+ * the outcome is a piece of art,followed by new line
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
