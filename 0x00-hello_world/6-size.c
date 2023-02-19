7cfa8fe35cb740ad0fa89e81dc88204b323d430b
Win2003 <winifredaniukwu@gmail.com>
2023-02-19 10:54:59 -0800

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,25 +2,24 @@
 
 /**
  * main - Entry point of program
- * purpose of program: To print sizes of data types on the computer
- * which it is compiled on
+ * purpose of the program: To print sizes of data types
+ * on the computer which it is compiled on
  * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	int a;
-	char b;
+	char a;
+	int b;
 	long int c;
 	long long int d;
 	float e;
 	double f;
 
-	printf("Size of int: %zu byte(s)\n", sizeof(a));
-	printf("Size of char: %zu byte(s)\n", sizeof(b));
+	printf("Size of char: %zu byte(s)\n", sizeof(a));
+	printf("Size of int: %zu byte(s)\n", sizeof(b));
 	printf("Size of long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(d));
 	printf("Size of float: %zu byte(s)\n", sizeof(e));
-	printf("Size of double: %zu byte(s)\n", sizeof(f));
+	printf("Size of double: %zu bytes\n", sizeof(f));
 	return (0);
 }
-
