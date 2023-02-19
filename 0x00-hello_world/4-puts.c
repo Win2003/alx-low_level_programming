3864f2ce2b3a43705d44c8f7a3c35a9018be625d
Win2003 <winifredaniukwu@gmail.com>
2023-02-18 18:44:46 -0800

    Using-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main: This is the entry point of the program.
+ * main - This is the entry point of the program.
  * purpose of program: To print exactly "Programming is like building a
  * multilingual puzzle using the puts function.
- * result = 0: Successful execution
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
