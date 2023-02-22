bf8716dd29370f129787b74a0f2e34d0633c6a5a
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 02:03:17 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of program
+ * purpose of program: To write a program using the _putchar function
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char a = 'A';
+
+	_putchar(a);
+	return (0);
+}
