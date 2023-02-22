4e3c64997ae6feed3b602147479809eb2da63bc3
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 12:47:36 -0800

    Alphas

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,5 @@
 #include "main.h"
-/**
- * main - Check the code
- * purpose of function: To print alphabet in lowercase
- * Return: Always 0
- */ 
+
 void print_alphabet(void)
 {
 	char a = 'a';
@@ -11,8 +7,6 @@ void print_alphabet(void)
 	while (a <= 'z')
 	{
 		_putchar(a);
+		a++
 	}
-	_putchar('\n');
-	return (0);
-}
-
+}	
