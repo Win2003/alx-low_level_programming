d7086f3fbc103417c19663e8fc9c2e70bdc0760f
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 04:08:24 -0800

    Good-file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Check the code
+ * purpose of function: To print alphabet in lowercase
+ * Return: Always 0
+ */ 
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
+
