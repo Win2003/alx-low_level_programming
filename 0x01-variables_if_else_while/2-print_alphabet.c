caba2bee89d4b837e8bb05bcda42032aa7833bc6
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 01:00:26 -0800

    letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,17 @@
 
 /**
  * main - Entry point of program
- * purpose of program: To print alphabet in lowercase
+ * purpose of program: To print the alphabet
  * Return: Always 0 (Successful)
  */
 int main(void)
 {
 	char a = 'a';
-	while (a <= 'z')
+
+	while (a >= 'z')
 	{
-		putchar(a);
+		putchar (a);
 		a++;
 	}
 	return (0);
-}	
+}
