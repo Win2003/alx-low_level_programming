e7a8d4fd6d7d82d5d515fadde8f38dc831a2f89d
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 00:34:28 -0800

    print-alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * purpose of program: To print alphabet in lowercase
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char a;
+	while (a = 'a' && a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	return (0);
+}	
