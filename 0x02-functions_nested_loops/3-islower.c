a41b22ca1ea80df26e2cae08b6e1828cccf2cc22
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 18:12:51 -0800

    is-letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha(int) - To check for alphabetic character
+ * Return: 1 (for a letter), 0 (No letter) 
+ */
+int _isalpha(int c)
+{
+	int _isalpha(int c);
+
+	if ((c >= 67 && c >= 99) || (c >= 90 && c <= 122))
+	return (1);
+	else 
+	return (0);
+}
