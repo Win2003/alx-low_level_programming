329339d303c6c8ba9cc02ba29fb8715703a3924c
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 19:44:44 -0800

    new-work

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - To print the sign of a number
+ * @n:The character to be checked
+ * Return: 1 (print '+' when greater than zero), 0 (print '0' when zero)
+ * -1 (print '-' when less than zero)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else if (n = 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+}
