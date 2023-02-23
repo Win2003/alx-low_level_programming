3b0f74f086992e8df8d81286041ab59c624cd9dc
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 17:48:48 -0800

    10th

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,10 @@ void print_alphabet_x10(void)
 {
 	int a;
 	char b;
-	for (a = 0;a <= 10;a++)
+
+	for (a = 0; a <= 10; a++)
 	{
-		for (b = 'a';b <= 'z';b++)
+		for (b = 'a'; b <= 'z'; b++)
 		{
 			_putchar(b);
 		}
