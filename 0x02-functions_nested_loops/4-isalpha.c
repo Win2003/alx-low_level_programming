6322c16ceeb0b255a04e92a2cb8e96bca7142fe7
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 19:19:19 -0800

    alphabes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha(int c) - To check for alphabetic character.
+ * @c: The character to be checked
+ * Return: 1 (alphabetic character), 0 (otherwise)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
