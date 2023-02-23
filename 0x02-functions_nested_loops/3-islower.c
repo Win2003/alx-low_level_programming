8e30ee0a3272b5e697a84eeda3bf86bb20f007e1
Win2003 <winifredaniukwu@gmail.com>
2023-02-22 18:55:01 -0800

    files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower(int c) - To check if int c is lowercase character
+ * _islower - To check if character c is lowercase character
  * Return: 1 for lowercase character or 0 for anything else
  */
 
