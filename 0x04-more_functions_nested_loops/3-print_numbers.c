05f90df52ead7e19b5fbe9f526d30ac42e168af5
Win2003 <winifredaniukwu@gmail.com>
2023-02-24 06:49:50 -0800

    nael

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** 
+ * print_numbers - To print numbers 0 through 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char A = '0';
+		
+		while ( A <= '9')
+		{
+			_putchar(A);
+		}
+		_putchar('\n');
+}
