9306cd7e3dd3654ded6038315055ffca9645a603
Win2003 <winifredaniukwu@gmail.com>
2023-02-24 06:02:33 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int mul(int a, int b)
+ * @a: the number to be multiplied
+ * @b: the number to be multiplied with
+ * Return: M ( the value of a * b)
+ */
+int mul(int a, int b)
+{
+	int M = a * b;
+	_putchar('\n');
+	return (M);
+}
