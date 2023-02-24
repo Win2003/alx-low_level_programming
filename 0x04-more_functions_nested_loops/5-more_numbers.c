b8536f5d39bf5474364eeebe43b5a48127c2c701
Win2003 <winifredaniukwu@gmail.com>
2023-02-24 12:36:12 -0800

    nesk

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - To print the number 0 - 14 ten times
+ * Result: Void
+ */
+void more_numbers(void)
+{
+	char a, b;
+	
+	for (a = '1'; a <= '10'; a++)
+	{
+		for (b = '0'; b <= '14'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
