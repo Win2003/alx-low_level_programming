4b46c050d066fae496c069a3152d79db274ed18b
Win2003 <winifredaniukwu@gmail.com>
2023-02-26 05:49:44 -0800

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,11 @@
  * main - To test the function that prints if integer is positive or negative
  * Return: 0 (Successful) 
  */
-int main(void)
+int main(void);
 {
 	int i;
 
 	i = 0;
 
 	positive_or_negative(i);
-
-	return (0);
 }
