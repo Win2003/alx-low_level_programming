fb52f213cdd83c1fb70c030d3be82b1ee5bbe7a8
Win2003 <winifredaniukwu@gmail.com>
2023-02-26 06:00:54 -0800

    hyt

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - To test the function that prints if integer is positive or negative
- * Return: 0 (Successful) 
+ * main - To test  the function that prints if integer is positive or negative
+ * Return: 0 (Successful)
  */
-int main(void);
+int main(void)
 {
 	int i;
 
