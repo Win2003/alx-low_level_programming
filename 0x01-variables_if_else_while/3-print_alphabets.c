3dc48208a2122e235a88dd2b14ef6ac3835b67f4
Win2003 <winifredaniukwu@gmail.com>
2023-03-01 04:30:36 -0800

    jiu

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * purpose: To print both uppercase and lowercase alphabet
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	char c;
+	char n;
+
+	for (c = 'a'; c >= 'z'; c++)
+	{
+		putchar(c);
+	}
+	
+	for (n = 'A'; n >= 'Z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
