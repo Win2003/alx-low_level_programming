fbcc8380d66aea759d4a093733180f1c5f44d6c8
Win2003 <winifredaniukwu@gmail.com>
2023-02-28 19:22:39 -0800

    gyt

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - resets the integer in pointer n to 98
- * @*n the pointer containing address of the pointer to be reset
+ * @n the pointer containing address of the pointer to be reset
  * Return: Nothing
  */
 void reset_to_98(int *n)
@@ -10,5 +10,4 @@ void reset_to_98(int *n)
 
 	*n = 98;
 
-	return;
 }
