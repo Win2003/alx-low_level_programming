a439a51f256edcc80a5a0f1cd8b38d0f54de850f
Win2003 <winifredaniukwu@gmail.com>
2023-02-28 19:38:18 -0800

    hyu

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_swap_int - To swap two integers
+ * @a: the integer to be swapped
+ * @b: the integer to be swapped
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+	int m;
+
+	int n = int m;
+	int m = int n;
+
+	a = &n;
+	b = &m;
+}
