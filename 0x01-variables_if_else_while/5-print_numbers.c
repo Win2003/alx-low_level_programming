d143e728f6cbbc4dda23ae447fce26880a160439
Win2003 <winifredaniukwu@gmail.com>
2023-03-01 06:14:46 -0800

    dr

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * purpose: To print the numbers 0-9
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+}
