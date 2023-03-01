9bbd6dc8d0ed2da03211e75c2a8bfbb25eb290ac
Win2003 <winifredaniukwu@gmail.com>
2023-02-28 19:25:31 -0800

    grt

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - resets the integer in pointer n to 98
- * @n the pointer containing address of the pointer to be reset
+ * @n: the pointer containing address of the pointer to be reset
  * Return: Nothing
  */
 void reset_to_98(int *n)
