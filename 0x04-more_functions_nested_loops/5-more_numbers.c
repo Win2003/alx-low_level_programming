6efa0f50e424eda5000e3c1e1d41990fd98df3bc
Win2003 <winifredaniukwu@gmail.com>
2023-03-02 07:27:34 -0800

    vfr

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * more_numbers - To print numbers from 0 to 14 ten times
+ * more_numbers - To print numbers 0 to 14 ten times
  * Return: nothing
  */
 void more_numbers(void)
 {
 	int a;
-	char b;
-	int c;
+	int b;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b > 9)
+			{
+				_putchar('1');
+				_putchar('0' + b % 10);
+				continue;
+			}
+			_putchar('\n');
 		}
-		for (c = 10; c <= 14; c++)
-		{
-			_putchar('0' + c);
-		}
-		_putchar('\n');
 	}
 }
