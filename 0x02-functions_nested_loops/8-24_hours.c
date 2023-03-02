8d76c7d046a659173108ad80f6ca3ddb7c138195
Win2003 <winifredaniukwu@gmail.com>
2023-03-01 18:55:40 -0800

    vcd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack-bauer - Prints every minute of the day of Jack Bauer
+ * jack_bauer - To print every minute of the day of jack bauer
+ * Return: nothing
  */
 void jack_bauer(void)
 {
@@ -11,23 +12,20 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if (a == 2 && b == 4)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				break;
-			}
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar('0' + a);
+						_putchar('0' + b);
+						_putchar(':');
+						_putchar('0' + c);
+						_putchar('0' + d);
+						_putchar('\n');
+					}
 				}
 			}
 		}
 	}
-}
-		
