01e23650c955646388c397baccd44a3cac675792
Win2003 <winifredaniukwu@gmail.com>
2023-03-02 10:56:05 -0800

    der

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,8 +2,22 @@
 
 /**
  * print_diagonal - To print a diagonal line on the terminal
+ * @n: The number of times the space is printed
  * Return: Void
  */
-void print_diagonal(int n);
+void print_diagonal(int n)
 {
-
+	int a;
+	
+	a = 1;
+	while (a <= n)
+	{
+		if (n > 0)
+		{
+			_putchar(' ');
+		}
+		_putchar(39);
+		m++;
+	}
+	_putchar('\n');
+}
