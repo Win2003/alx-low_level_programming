cbc718ddf057a197c70594379a6cbabd4cdc2013
Win2003 <winifredaniukwu@gmail.com>
2023-03-03 04:55:47 -0800

    fd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print-rev - To print a string in reverse
+ * @s: the string to be printed
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		s++;
+	}
+	while (*s != '\0' && s >= *s)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n')
+}
