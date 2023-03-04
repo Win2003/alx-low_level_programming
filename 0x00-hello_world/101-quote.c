48388bbcc8a1880a73778c8e1d10947cdc0514be
Win2003 <winifredaniukwu@gmail.com>
2023-03-04 05:24:35 -0800

    sew

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * purpose: To print a string
+ * Return: Always 1
+ */
+int main(void)
+{
+	char t[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *d = char t[];
+
+	while (*d != '\0')
+	{
+		putchar(*d);
+	}
+	putchar('\n');
+}
