489df5a790bbe66c335c0d92efead5c4473abeae
Win2003 <winifredaniukwu@gmail.com>
2023-03-04 05:40:31 -0800

    vtr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of program
- * purpose: To print a string
- * Return: Always 1
+ * main - To print string to standard error
+ * Return: always 1
  */
 int main(void)
 {
-	char t[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	char *d = char t[];
-
-	while (*d != '\0')
-	{
-		putchar(*d);
-		d++;
-	}
-	putchar('\n');
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	 return (1);
 }
