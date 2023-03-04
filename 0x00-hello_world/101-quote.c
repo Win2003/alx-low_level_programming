2bc669655c772a324b93232511686ea428941557
Win2003 <winifredaniukwu@gmail.com>
2023-03-04 05:59:20 -0800

    de

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - To print string to standard error
- * Return: always 1
+ * main - T o print a string to standard error
+ * Return: Always 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora
-Korpar, 2015-10-19\n", stdout);
- return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 20", stdout);
+	fputs("15-10-19\n", stdout);
+	return (1);
 }
