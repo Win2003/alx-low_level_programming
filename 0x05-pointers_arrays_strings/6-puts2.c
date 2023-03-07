cd0a35f29d66fc6507df055814e6ad8110759def
Win2003 <winifredaniukwu@gmail.com>
2023-03-07 09:31:54 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - To print every otherr character of a string
+ * @str: The string to be printed
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
