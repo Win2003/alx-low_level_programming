1b443cf6e72c482cce6e67101524a31d0e42aa40
Win2003 <winifredaniukwu@gmail.com>
2023-03-07 07:42:37 -0800

    tyu

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,8 @@
-void positive_or_negative()
-int largest_number(int a, int b, int c)
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int _putchar(char c);
 int largest_number(int a, int b, int c);
